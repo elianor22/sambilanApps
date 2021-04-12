79c61bb73aa0a216a98eceb635e350696d757c92
Aji Nugraha <76178301+elianor22@users.noreply.github.com>
2021-04-12 22:24:24 +0700

    update many plugin and screens

--- App.js
+++ App.js
@@ -11,18 +11,18 @@ import {ExploreScreen,DetailScreen, CheckoutScreen} from './src/screen'
 export default function App() {
   const Stack = createStackNavigator();
   return (
-    // <NavigationContainer>
-    //   <Stack.Navigator
-    //     screenOptions={{
-    //       headerShown: false,
-    //     }}>
-    //     <Stack.Screen name="Home" component={Home} />
-    //     <Stack.Screen name="Explore" component={ExploreScreen} />
-    //     <Stack.Screen name="Detail" component={DetailScreen} />
-    //   </Stack.Navigator>
-    // </NavigationContainer>
-    <View style={{flex:1}}>
-      <CheckoutScreen />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Explore" component={ExploreScreen} />
+        <Stack.Screen name="Detail" component={DetailScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  //   <View style={{flex:1}}>
+  //     <CheckoutScreen />
+  //   </View>
   );
 }
